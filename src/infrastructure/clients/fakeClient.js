d086d4680959cbe06568f5552ffcd9a75560ec10
Leandro Comparin <lpcomparing@gmail.com>
2022-08-22 14:10:28 -0300

    rm: js dist files

--- src/infrastructure/clients/fakeClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FakeClient = void 0;
-class FakeClient {
-    mimic(o) {
-        return Promise.resolve(o);
-    }
-}
-exports.FakeClient = FakeClient;
